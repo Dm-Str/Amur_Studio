3ca7bae641e01aea52e3d015488e07804c870798
DStr <dist812@yandex.ru>
2024-11-27 08:15:37 +0300

    Add notifications module

---
+++ nail_studio/notifications.py
@@ -0,0 +1,13 @@
+from .models import Courses, Notifications, Discounts
+
+
+def check_course_notifications():
+    pass
+
+
+def check_discount_notifications():
+    pass
+
+
+def check_new_courses_notifications():
+    pass
