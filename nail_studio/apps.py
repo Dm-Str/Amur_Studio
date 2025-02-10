61af7de20f29ce5d5f9da067c7b3b75217ff8449
DStr <dist812@yandex.ru>
2025-02-10 15:35:17 +0300

    Added signals

--- nail_studio/apps.py
+++ nail_studio/apps.py
@@ -5,3 +5,6 @@ class NailStudioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'nail_studio'
     verbose_name = 'Стрела Амура'
+
+    def ready(self):
+        import nail_studio.signals
